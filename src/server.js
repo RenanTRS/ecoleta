63bfd3de8fdefaae369dda88436283794248d957
Renan Souza <renantrsouza@gmail.com>
2021-08-16 15:28:10 -0400

    modificando os arquivos para o formato nunjucks

--- src/server.js
+++ src/server.js
@@ -4,18 +4,28 @@ const server = express();
 //Configurar pasta public
 server.use(express.static("public"));
 
+//Utilizando template engine
+const nunjucks = require("nunjucks");
+nunjucks.configure("src/views", {
+    express: server,
+    noCache: true
+});
+
 //Configurar caminhos da minha aplicação
 //página inicial
 //req: requisição
 //res: resposta
 server.get("/", (req, res)=>{
-    res.sendFile(__dirname + "/views/index.html");
+    //res.sendFile(__dirname + "/views/index.html"); //Sem nunjucks
+    return res.render("index.html", {title: "Alguma coisa aqui"}); //Com nunjucks
 });
 server.get("/create-point", (req, res)=>{
-    res.sendFile(__dirname + "/views/create-point.html");
+    //res.sendFile(__dirname + "/views/create-point.html"); //Sem nunjucks
+    return res.render("create-point.html"); //Com nunjucks
 });
 server.get("/search", (req, res)=>{
-    res.sendFile(__dirname + "/views/search-results.html");
+    //res.sendFile(__dirname + "/views/search-results.html"); //Sem nunjucks
+    return res.render("search-results.html"); //Com nunjucks
 });
 //ligar o servidor
 server.listen(5500);
